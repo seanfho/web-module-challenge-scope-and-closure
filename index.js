3a6d653ce6e4122a1ead7b1cd5f0a34011083336
Sean Ho <seanfho@gmail.com>
2021-03-22 11:56:41 -0400

    Solution to Task4 added to index.js

--- index.js
+++ index.js
@@ -114,9 +114,15 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(callbackFunction) {
+  let homeScore = callbackFunction();
+  let awayScore = callbackFunction();
+  return {Home: homeScore, Away: awayScore};
 }
+
+console.log("Task 4 Test 1:", getInningScore(inning));
+console.log("Task 4 Test 1:", getInningScore(inning));
+console.log("Task 4 Test 1:", getInningScore(inning));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
